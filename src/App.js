cd4ff975780e9e176b048dfc628da6d5b01dff6c
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-01-09 13:41:28 -0500

    added some components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import './App.css';
+import stockPhoto from './images/stock.jpg';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1 className='text-blue-500'>hello</h1>
+    <>
+    <div className='m-6 flex'>
+      <img className='rounded-full m-2 w-16 h-16 object-cover shadow-lg' src={stockPhoto} alt='random person' />
+      <img className='rounded-lg w-16 h-16 object-cover shadow-lg' src={stockPhoto} alt='random person' />
+      <img className='rounded-full w-32 h-32 object-cover shadow-lg' src={stockPhoto} alt='random person' />
+      <img className='rounded-lg w-32 h-32 object-cover shadow-lg' src={stockPhoto} alt='random person' />
     </div>
+
+    <div></div>
+    </>
   );
 }
 
